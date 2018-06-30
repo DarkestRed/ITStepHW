2acbcd87498dd8209ea0ebbe5ecbff10a70cac17
DarkestRed <40231252+DarkestRed@users.noreply.github.com>
2018-06-30 17:14:22 +0300

    Update script.js

--- Cycles/Task 5/script.js
+++ Cycles/Task 5/script.js
@@ -2,7 +2,7 @@ var num = parseInt(prompt("Введите число:"));
 var summa = 0;
 
 while (num > 0) {
-    summa = summa + num % 10;
+    summa += num % 10;
     num = parseInt(num / 10);
 }
 alert("Сумма цифр: " + summa);
