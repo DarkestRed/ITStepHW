a1089f1eda41aeb815961d29bafcf09c6e082f30
DarkestRed <darkestred16@gmail.com>
2018-06-30 22:41:41 +0300

    Ugaday chislo

--- 29.06 Cycles/Task 02, Ugaday chislo/script.js
+++ 29.06 Cycles/Task 02, Ugaday chislo/script.js
@@ -2,27 +2,31 @@ function rand(a, b) {
     return Math.round(Math.random() * (b - a)) + a;
 }
 
-var tryes = 5;
+var tries = 5;
 var x;
 
-while (tryes > 0) {
-    if (tryes === 5) x = rand(1, 100);
-    var ans = parseInt(prompt("Введите число от 1 до 100 " + x));
-    tryes--;
+while (tries > 0) {
+    if (tries === 5) x = rand(1, 100);
+    var ans = parseInt(prompt("Введите целое число от 1 до 100" + " (Для проверки, загаданное число " + x + ")"));
+    if (ans > 100 || ans === 0 || ans !== ans) {
+        alert("Вы ввели некорректный ответ, попробуйте еще раз.");
+        continue;
+    }
+    tries--;
     if (ans === x) {
-        if (confirm("Правильно! Нужное число " + x + "\nВы выиграли!\nХотите сыграть еще?")) tryes = 5;
+        if (confirm("Правильно! Нужное число " + x + "\nВы выиграли!\nХотите сыграть еще?")) tries = 5;
         else {
             alert("Спасибо за игру!");
             break;
         }
     }
-    else if (tryes === 0) {
-        if (confirm("Попытки исчерпаны!\nВы проиграли!\nНужное число было " + x + "\nХотите сыграть еще?")) tryes = 5;
+    else if (tries === 0) {
+        if (confirm("Попытки исчерпаны!\nВы проиграли!\nНужное число было " + x + "\nХотите сыграть еще?")) tries = 5;
         else {
             alert("Спасибо за игру!");
             break;
         }
     }
-    else if (ans > x) alert("Нет, слишком много!\nОсталось попыток: " + tryes);
-    else if (ans < x) alert("Нет, слишком мало!\nОсталось попыток: " + tryes);
+    else if (ans > x) alert("Нет, слишком много!\nОсталось попыток: " + tries);
+    else if (ans < x) alert("Нет, слишком мало!\nОсталось попыток: " + tries);
 }
