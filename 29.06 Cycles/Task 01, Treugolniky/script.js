9ad8511c3f65f40293dc46be7ece5f3aef12b37f
DarkestRed <darkestred16@gmail.com>
2018-07-02 02:07:33 +0300

    Treugolniky

---
+++ 29.06 Cycles/Task 01, Treugolniky/script.js
@@ -0,0 +1,17 @@
+var height = parseInt(prompt("Введите высоту треугольника"));
+
+for (var row = 0; row < height; row++) {
+    for (var symbol = 1; symbol <= height + row; symbol++) {
+        if (symbol >= height - row) document.write("* ");
+        else document.write("&nbsp; &nbsp;");
+    }
+    document.write("<br>");
+}
+
+for (var row = 0; row < height; row++) {
+    for (var symbol = 1; symbol <= height + row; symbol++) {
+        if (symbol === height - row || symbol === height + row || row === height - 1) document.write("* ");
+        else document.write("&nbsp; &nbsp;");
+    }
+    document.write("<br>");
+}
